3746db8215766e90afb2f802529f8a7955374176
James <jsmchugh@gmail.com>
2018-08-11 21:41:00 +1000

    Credit Entry class

---
+++ Assets/Scripts/CreditEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Credit Entry")]
+public class CreditEntry : ScriptableObject
+{
+	public string creditName;
+
+	public string twitterHandle;
+
+	public string role;
+
+	public string website;
+}
